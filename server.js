32724ce5fa83718ed792ac5c86d48fd5481b5a49
Aaron Everette <aeverette0120@gmail.com>
2022-08-11 21:36:20 -0400

    create and new routes complete

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
-// MIDDLEWARE
+
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-// MIDDLEWARE
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
 
 // ROUTES
@@ -23,10 +23,6 @@ app.get('/', (req, res) => {
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
   
-// 404 Page
-app.get('*', (req, res) => {
-  res.send('404')
-})
 
 // LISTEN
 app.listen(PORT, () => {

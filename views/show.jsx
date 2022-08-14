09e87d2cf21dfad88d921d51aaba540704d308ee
Aaron Everette <aeverette0120@gmail.com>
2022-08-14 13:31:17 -0400

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -8,6 +8,8 @@ function Show({ bread, index }) {
     <Default>
       <h3>{bread.name}</h3>
 
+      <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
       <form action={`/breads/${index}?_method=DELETE`} method="POST">
         <input type="submit" value="DELETE" />
       </form>

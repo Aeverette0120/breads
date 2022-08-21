e9bf856f86f368f37c1521e87273a9b887cd6950
Aaron Everette <aeverette0120@gmail.com>
2022-08-21 10:38:49 -0400

    added /data/seed route

--- views/show.jsx
+++ views/show.jsx
@@ -8,11 +8,12 @@ function Show({ bread, index }) {
     <Default>
       <h3>{bread.name}</h3>
 
-      <a href={`/breads/index/edit`}><button>Edit</button></a>
+      <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
 
-      <form action={`/breads/index}?_method=DELETE`} method="POST">
-        <input type="submit" value="DELETE" />
-      </form>
+      <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
+  <input type='submit' value="DELETE"/>
+</form>
+
 
       <p>
         and it

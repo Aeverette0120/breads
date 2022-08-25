ac4f52ba20e62d45f0827acc49d8f287d1a4f22a
Aaron Everette <aeverette0120@gmail.com>
2022-08-25 19:06:09 -0400

    created and tested the delete button

--- views/bakerShow.jsx
+++ views/bakerShow.jsx
@@ -5,13 +5,6 @@ function Show({ baker }) {
   return (
     <Default>
       <h3>{baker.name}</h3>
-      <h3>Breads {baker.name} has baked</h3>
-      <ul>
-        {baker.breads.map((bread) => {
-          return <li key={bread.id}>{bread.name}</li>;
-        })}
-      </ul>
-
       <p>
         {baker.name} has been baking with us since{" "}
         {baker.startDate.getFullYear()}
@@ -19,6 +12,18 @@ function Show({ baker }) {
       <p>
         About {baker.name}: {baker.bio}
       </p>
+      <h3>Breads {baker.name} has baked</h3>
+      <ul>
+        {baker.breads.map((bread) => {
+          return <li key={bread.id}>{bread.name}</li>;
+        })}
+      </ul>
+      <form action={`/bakers/${baker.id}?_method=DELETE`} method="POST">
+  <input type="submit" value="DELETE"/>
+</form>
+
+
+      
     </Default>
   );
 }
